5c89a314bee289e0e7bcc9c63de846fdce0f04e0
VCGamers <97363866+vcgamers@users.noreply.github.com>
2025-07-03 01:45:01 +0700

    Initial commit add ip 2

--- print-railway-ip.js
+++ print-railway-ip.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 async function fetchPublicIP() {
   try {
     // Gunakan layanan fetch IP yang aman dan cepat
-    const { data } = await axios.get('futurepilot-production.up.railway.app');
+    const { data } = await axios.get('https://api.ipify.org');
     console.log(`\n🚀 IP Publik Railway-mu: \x1b[32m${data}\x1b[0m`);
     console.log('\n✅ Masukkan IP ini ke Binance API whitelist!');
   } catch (e) {
